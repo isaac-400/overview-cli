7c94159ed9f9721e4de881592c1d61854f267cc0
Isaac Feldman <isaac.c.feldman.23@dartmouth.edu>
2020-06-14 21:08:15 -0400

    added weather feature

--- overview.py
+++ overview.py
@@ -1,9 +1,6 @@
 #Author: Isaac Feldman (isaac.c.feldman.23@dartmouth.edu)
 
-import os, argparse, feedparser
-from os import path
-parser = argparse.ArgumentParser()
-parser.parse_args()
+import os, feedparser, requests, json
 
 def get_top_news(n):
     """
@@ -19,12 +16,36 @@ def get_top_news(n):
         headlines[title] = link
     return headlines
 
+def get_weather(loc):
+    access_key = "426c2cdfb8d60e4504479e3814e21802"
+    response = requests.get("http://api.weatherstack.com/current?"+"access_key="+access_key+"&query="+loc)
+    content = json.loads(response.content)
+
+    location = content['location']['name']
+    region = content['location']['region'] # Todo: Why is this in quotes?
+    temp = content['current']['temperature']
+    desc = content['current']['weather_descriptions']
+    precip = content['current']['precip']
+
+    if content['request']['unit'] == 'm':
+        unit = "C"
+    else:
+        unit = "F"
+
+    print("The weather in %s, %r is:" % (location, region))
+    print("Temp: %r%s" %(temp, unit))
+    print("Chance of Precipitation: %s percent" % precip)
+    print(", ".join(desc))
+
+
 
 def glance():
     print("Here are the top stories of the day:")
     stories = get_top_news(5)
     for line in stories.keys():
         print(line)
+
+    get_weather("New York")
 
 if __name__ == "__main__":
     glance()
